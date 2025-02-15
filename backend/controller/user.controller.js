85e655eae195a017ef4a53e73465470faea37356
Vishal Aggarwal <007vishal.aggarwal@gmail.com>
2025-02-15 23:22:19 +0530

    Setting up backend

---
+++ backend/controller/user.controller.js
@@ -0,0 +1,92 @@
+import { User } from "../model/user.model.js";
+import bcrypt from "bcrypt";
+import { generateToken } from "../utils/generateToken.js";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "Something is missing",
+        success: false,
+      });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(200).json({
+        message: "User already exist",
+        success: false,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return res.status(201).json({
+      message: "User created",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      message: "Failed to register",
+      success: false,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "something is missing",
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Incorrect Email",
+        success: false,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        message: "Password or Email does not match",
+        success: false,
+      });
+    }
+
+    generateToken(res, user, `Welcome back ${user.name}`);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Failed to login",
+      success: false,
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+      message: "Logout Successfull",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to logout",
+    });
+  }
+};
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./database/db.js";
+import userRoute from "./routes/routes.js";
+
+dotenv.config({});
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "*" }));
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+//API's
+app.use("/api/v1/user", userRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
